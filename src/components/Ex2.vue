4c10d3b058ed46ae9c9179fc498dcd8941e7ab20
joshuayeo2024 <joshua.yeo.2024@smu.edu.sg>
2025-10-28 06:05:49 +0000

    working ex1 and ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -28,12 +28,15 @@
             .catch(error => {
                 this.posts = [{ entry: 'There was an error: ' + error.message }]
             })
+        },
+        components: {
+            blogPost
         }
     }
 </script>
 
 <template>
    <!-- TODO: make use of the 'blog-post' component to display the blog posts -->
-
+    <blogPost v-for="post of posts" :subject="post.subject" :mood="post.mood" :entry="post.entry"></blogPost>
 </template>
 
