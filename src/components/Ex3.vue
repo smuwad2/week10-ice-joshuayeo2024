18e0883be99e6d722137b747b6dcbe508a96f6ce
joshuayeo2024 <joshua.yeo.2024@smu.edu.sg>
2025-10-28 06:53:18 +0000

    ex3 completed

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -35,13 +35,31 @@
         methods: {
             deletePost(id) {
                 // TODO: Complete the delete method
+                axios.get(`${this.baseUrl}/deletePost`,{
+                    params:{
+                        id:id
+                    }
+                }).then(response =>{
+                    console.log(response.data.message)
+                    this.posts = this.posts.filter(post => post.id!=id)
+                }).catch(error => {
+                    console.log(error)
+                })
+
+                
             }
+        },
+        components:{
+            blogPost
         }
     }
 </script>
 
 <template>
    <!-- TODO: make use of the 'blog-post' component to display the blog posts -->
-
+    <blogPost v-for="post of posts" :subject="post.subject" :mood="post.mood" :entry="post.entry">
+        <button class="btn btn-primary" @click="deletePost(post.id)">Delete</button>
+    </blogPost>
+    
 </template>
 
--- src/components/subcomponents/BlogPost2.vue
+++ src/components/subcomponents/BlogPost2.vue
@@ -2,12 +2,31 @@
     export default {
         // Add code if/as necessary
         // Hint: Add properties subject, entry and mood
+        props:{
+            subject: String,
+            entry: String,
+            mood: String,
+            id: Number
+        },
+        computed: {
+            moodLower () {
+                return this.mood.toLowerCase();
+            }
+        },
     }
 </script>
 
 
 <template>
     <!-- TODO: add your template code here Use boostrap card --> 
+<div class="card" style="width: 18rem;">
+  <img :src="`/assets/${moodLower}.png`" class="card-img-top" alt="Card image">
+  <div class="card-body text-center">
+    <h5 class="card-title">{{ subject }}</h5>
+    <p class="card-text">{{ entry }}</p>
+    <slot />
+  </div>
+</div>
 
 </template>
 
